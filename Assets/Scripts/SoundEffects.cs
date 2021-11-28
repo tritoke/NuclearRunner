325c71670beec1b8fcbbcc4b8a9cdc89b952b895
george.tarleton <george.tarleton@student.manchester.ac.uk>
2021-11-28 12:22:09 +0000

    Sounds now work!

---
+++ Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioSource backgroundMotor;
+
+    void Start()
+    {
+        backgroundMotor.loop = true;
+
+        playBackground();
+    }
+
+    public void playBackground()
+    {
+        backgroundMotor.Play();
+    }
+
+}
